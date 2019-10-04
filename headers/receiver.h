421d596392b206755f14cd9ea0aa9b196f3ab0ea
skjdbg <thomas.heuschling@skynet.be>
2019-10-04 22:59:40 +0200

    split recv into subfunctions, added buff allocator

--- headers/receiver.h
+++ headers/receiver.h
@@ -2,13 +2,29 @@
 #include <sys/socket.h>
 #include "../headers/packet.h"
 
+/**
+ * ## Use :
+ *
+ * 
+ * 
+ * ## Arguments :
+ *
+ * - `argument` - a pointer to the address you want to listen to
+ *
+ * ## Return value:
+ *
+ * the socketfd if the process completed successfully. -1 otherwise.
+ * If it failed, errno is set to an appropriate error. 
+ *
+ */
+int make_listen_socket(const struct sockaddr *src_addr, socklen_t addrlen);
+
 //TODO1: gérer errno, 
 //TODO2: potentiellement passer le socket en argument pour le réutiliser ou autre
-//TODO3: avoir un buffer uint8_t permanent pour réceptionner les messages sans allocations
 //TODO4: rajouter un paramètre payload pour le passer en argument à unpack
 //TODO5: rename function =p
 /**
- *  ## Use :
+ * ## Use :
  *
  * creates a socket, listens and unpacks a message.
  * 
@@ -20,10 +36,43 @@
  * - `sender_addr` - pointer to a buffer in which the source of the packet will be stored
  * - `sender_addr_len` - before the call : points to the size of the buffer sender_addr
  *                     - after the call : points to the size of the address stored in sender_addr
+ * - 'packet_received' - a buffer capable of containing a packet (this is so that we don't need to reallocate memory)
  * 
  * ## Return value:
  *
  * 0 if the process completed successfully. -1 otherwise.
  * If it failed, errno is set to an appropriate error. 
  */
- int recv_and_handle_message(const struct sockaddr *src_addr, socklen_t addrlen, packet_t* packet, struct sockaddr * sender_addr, socklen_t* sender_addr_len);
+int recv_and_handle_message(const struct sockaddr *src_addr, socklen_t addrlen, packet_t* packet, struct sockaddr * sender_addr, socklen_t* sender_addr_len, uint8_t* packet_received);
+
+/**
+ * ## Use :
+ *
+ * allocates a list containing 'number' 528 byte uint8_t* buffers to store incoming packets
+ * 
+ * ## Arguments :
+ *
+ * - `number` - the number of buffer you want to allocate
+ * - 'buffers' - the pointer to the list where the buffers will be stored
+ *
+ * ## Return value:
+ * 
+ * 0 if the process completed successfully. -1 otherwise.
+ * If it failed, errno is set to an appropriate error.
+ */
+ int alloc_reception_buffers(int number, uint8_t** buffers);
+
+/**
+ * ## Use :
+ *
+ * deallocates a list of number buffers of 528 bytes
+ * 
+ * ## Arguments :
+ *
+ * - `number` - the number of buffer that were allocated
+ * - 'buffers' - pointer to the list of buffers to dealocate
+ *
+ * ## Return value:
+ * 
+ */
+void dealloc_reception_buffers(int number, uint8_t** buffers);
--- src/receiver.c
+++ src/receiver.c
@@ -6,28 +6,61 @@
 #include "../headers/receiver.h"
 #include "../headers/packet.h"
 
-/*
- * Refer to headers/receiver.h
- */
-int recv_and_handle_message(const struct sockaddr *src_addr, socklen_t addrlen, packet_t* packet, struct sockaddr * sender_addr, socklen_t* sender_addr_len) {
-    // TODO: Create a IPv6 socket supporting datagrams
-    int sock = socket(AF_INET6, SOCK_DGRAM,0);
-    if(sock < 0){ // j'ai pas fait de errno parce que ca restera probablement pas ici
+int make_listen_socket(const struct sockaddr *src_addr, socklen_t addrlen){
+    int socket = socket(AF_INET6, SOCK_DGRAM,0);
+    if(socket < 0){ //errno handeled by socket
         return -1;
     }
 
-    // TODO: Bind it to the source
-    int err = bind(sock, src_addr, addrlen);
+    int err = bind(socket, src_addr, addrlen);
     if(err == -1){
-        return -1; // j'ai pas fait de errno parce que ca restera probablement pas ici
+        return -1; // errno handeled by bind
+    }
+    return socket;
+}
+
+/*
+ * Refer to headers/receiver.h
+ */
+int alloc_reception_buffers(int number, uint8_t** buffers){
+    if(number < 1){
+        return 0;
     }
 
-    // TODO: Receive a message through the socket
-    uint8_t* packet_received = (uint8_t*) calloc(528, sizeof(uint8_t));//je sais mais c'est temporaire jusqu'à ce que le reste du code soit là =p
+    for(int i = 0; i < number; i++){
+        *(buffers+i*528) = calloc(528,sizeof(uint8_t));
+        if(*(buffers+i*528) == NULL){
+            dealloc_reception_buffers(i-1, buffers);
+            errno = FAILED_TO_ALLOCATE;
+            return -1
+        }
+    }
 
+    return 0;
+}
+
+/*
+ * Refer to headers/receiver.h
+ */
+int dealloc_reception_buffers(int number, uint8_t** buffers){
+    if(number > 1){
+        for(int i = 0; i < number; i++){
+            free(*(buffers+i*528));
+        }
+    }
+}
+
+/*
+ * Refer to headers/receiver.h
+ */
+int recv_and_handle_message(const struct sockaddr *src_addr, socklen_t addrlen, packet_t* packet, struct sockaddr * sender_addr, socklen_t* sender_addr_len, uint8_t* packet_received) {
+
+     int socket = socket(src_addr, addrlen);
+
+    // TODO: Receive a message through the socket
     ssize_t n_received = recvfrom(sock, packet_received, sizeof(packet_t), 0, sender_addr, sender_addr_len);
     if(n_received == -1){
-        return -1; 
+        return -1;
     }
     
     if(alloc_packet(packet) == -1 && errno != ALREADY_ALLOCATED){ //checking if packet has been allocated
