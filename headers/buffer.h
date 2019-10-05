7a278fdaa08c3b4b9bd49a0ba8199b3e5a57151d
skjdbg <thomas.heuschling@skynet.be>
2019-10-05 20:34:10 +0200

    started the implementation. IM DONE FOR THE DAY

---
+++ headers/buffer.h
@@ -0,0 +1,72 @@
+#ifndef BUFFER_H
+
+#define BUFFER_H
+
+#define MAX_WINDOW_SIZE 31
+
+#include "packet.h"
+#include <pthread.h>
+#include <stdlib.h>
+
+typedef struct node{
+    packet_t *packet;
+    bool used;
+    pthread_mutex_t mut;
+}node_t;
+
+typedef struct buf{
+    uint8_t last_read;
+    uint8_t last_written;
+    node_t nodes[MAX_WINDOW_SIZE];
+}buf_t;
+
+
+/**
+ * ## Use :
+ * 
+ * ## Arguments :
+ *
+ * - `buf` - 
+ *
+ * ## Return value:
+ * 
+ * 0 if the process completed successfully. -1 otherwise.
+ * If it failed, errno is set to an appropriate error.
+ */
+int create_buffer(buf_t *buf);
+
+/**
+ * ## Use :
+ * 
+ * ## Arguments :
+ *
+ * - `buf` - 
+ *
+ * ## Return value:
+ * 
+ * 0 if the process completed successfully. -1 otherwise.
+ * If it failed, errno is set to an appropriate error.
+ */
+void deallocate_buffer(buf_t *buf);
+
+/**
+ * ## Use :
+ * 
+ * returs the 5 least significant bits of `seqnum`
+ * 
+ * this value is always between 0 and 31 and never returns two times the same number if given. this allows us to simply
+ * store the packet at the index returned by this function without 
+ * parsing said buffer
+ * 
+ * ## Arguments :
+ *
+ * - `seqnum` - 
+ *
+ * ## Return value:
+ * 
+ * 0 if the process completed successfully. -1 otherwise.
+ * If it failed, errno is set to an appropriate error.
+ */
+inline uint8_t hash(uint8_t seqnum) __attribute__((always_inline)); //"hash" we know it's not really one but here we go
+
+#endif
---
+++ src/buffer.c
@@ -0,0 +1,47 @@
+#include "../headers/buffer.h"
+#include "../headers/errors.h"
+#include <pthread.h>
+#include <errno.h>
+
+/*
+ * Refer to headers/buffer.h
+ */
+int create_buffer(buf_t *buf) {
+    buf = (buf_t *) calloc((size_t) 1, sizeof(buf_t));
+    if(buf == NULL) {
+        errno = FAILED_TO_ALLOCATE;
+        return -1;
+    }
+    buf->last_read = 0;
+    buf->last_written = 0;
+
+    for(int i = 0; i < MAX_WINDOW_SIZE; i++){
+        if(pthread_mutex_init(&(buf->nodes[i].mut), NULL) != 0) {
+            deallocate_buffer(buf);
+            //errno
+            return -1;
+        }
+        if(alloc_packet(buf->nodes[i].packet) != 0) {
+            deallocate_buffer(buf);
+            return -1;
+        }
+    }
+    return 0;
+}
+
+/*
+ * Refer to headers/buffer.h
+ */
+void deallocate_buffer(buf_t *buf) {
+    if(buf == NULL) {
+        errno = NULL_ARGUMENT;
+        return ;
+    }
+}
+
+/*
+ * Refer to headers/buffer.h
+ */
+uint8_t hash(uint8_t number) {
+    return number & 0b00011111;
+}
